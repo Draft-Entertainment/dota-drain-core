4422e4c29e1c7eefc02ba8af4ece50c2783aa520
El <lomashonok@inbox.lv>
2020-01-03 09:47:45 +0200

    #3 Add Data Context project

---
+++ DotaDrainCore.DataContext/Interfaces/IDataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotaDrainCore.DataContext.Interfaces
+{
+    public interface IDataContext
+    {
+        
+    }
+}
