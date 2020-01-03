4b86f04d755399bd8aba27f22e0a25a1161453d5
El <lomashonok@inbox.lv>
2020-01-03 09:56:45 +0200

    #4 Add Entities project and content

---
+++ DotaDrainCore.Entities/Match.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotaDrainCore.Entities
+{
+    public class Match : Entity
+    {
+    }
+}
---
+++ DotaDrainCore.Entities/WeightConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotaDrainCore.Entities
+{
+    public class WeightConfiguration : Entity
+    {
+    }
+}
