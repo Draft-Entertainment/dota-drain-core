093e57456da4cc4453a5bc37833eae090b3ed7ea
El <lomashonok@inbox.lv>
2020-01-15 21:15:14 +0200

    #6 Steam Data collercot service logic

--- DotaDrainCore.Entities/Player.cs
+++ DotaDrainCore.Entities/Player.cs
@@ -6,8 +6,8 @@ namespace DotaDrainCore.Entities
 {
     public class Player: Entity
     {
-        public int PlayerId { get; set; }
-        public int SteamAccountId { get; set; }
+        public uint PlayerId { get; set; }
+        public uint SteamAccountId { get; set; }
         public string Name { get; set; }
         public bool HasDotaPlus { get; set; }
     }
